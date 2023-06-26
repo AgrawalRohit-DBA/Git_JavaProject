2e8a54d8620bcc168842ccddfe31a5d880997ded
Sneha Varude <snehavarude81@gmail.com>
2023-06-26 18:09:50 +0530

    New project created

---
+++ src/Test/Add.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c= a+b;
+		System.out.println("Addition is"+c);
+
+	}
+
+}
