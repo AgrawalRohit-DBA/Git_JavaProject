dfa5b3d9ee6c10a44df31f829c2427aaa2853621
Sneha Varude <snehavarude81@gmail.com>
2023-06-26 18:13:08 +0530

    New Class Added

---
+++ src/Test/Sub.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=10;
+		int c= a-b;
+		System.out.println("Sub is"+c);
+
+
+	}
+
+}
